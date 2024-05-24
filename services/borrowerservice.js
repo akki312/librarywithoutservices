2c9a89bc11d409e2f8e41021063eef73e1d5c41f
akki312 <akshithsistla@gmail.com>
2024-05-24 13:35:45 +0530

    borrowerservice.js

--- services/borrowerservice.js
+++ services/borrowerservice.js
@@ -1,12 +1,20 @@
 const Borrower = require('../models/borrower');
 
 async function createBorrower(borrowerData) {
-  const borrower = new Borrower(borrowerData);
-  return await borrower.save();
+  try {
+    const borrower = new Borrower(borrowerData);
+    return await borrower.save();
+  } catch (error) {
+    throw new Error('Could not create borrower: ' + error.message);
+  }
 }
 
 async function getBorrowerById(id) {
-  return await Borrower.findById(id);
+  try {
+    return await Borrower.findById(id);
+  } catch (error) {
+    throw new Error('Could not find borrower: ' + error.message);
+  }
 }
 
 module.exports = {
