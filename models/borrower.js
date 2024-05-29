d56f6777da8d44709c9edab137321a1c9d6aca1d
akki312 <akshithsistla@gmail.com>
2024-05-29 10:58:32 +0530

    borrower.js

--- models/borrower.js
+++ models/borrower.js
@@ -1,10 +1,15 @@
+// models/borrower.js
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const borrowerSchema = new Schema({
   name: { type: String, required: true },
-  contact: { type: String, required: true },
-  email: { type: String, required: true }
+  email: { type: String, required: true, unique: true },
+  borrowedBooks: [{ type: Schema.Types.ObjectId, ref: 'Book' }],
+  fine: { type: Number, default: 0 }
 });
 
-module.exports = mongoose.model('borrower', borrowerSchema);
+const Borrower = mongoose.model('Borrower', borrowerSchema);
+
+module.exports = Borrower;
